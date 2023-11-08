9926b9d54e41985392cc8f5287a3a08a5ee89d55
hyunjin <alfma123@naver.com>
2023-11-08 17:34:09 +0900

    fix: user property rewhisper로 잘못 지정된 부분 수정

--- src/models/like.entity.ts
+++ src/models/like.entity.ts
@@ -1,16 +1,16 @@
 import { Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
-import { User, Whisper } from '.';
+import { Whisper } from '.';
 
 @Entity('likes')
 export class Like {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @ManyToOne(() => User, (user) => user.rewhispers)
+  @ManyToOne(() => User, (user) => user.likes)
   @JoinColumn({ name: 'user_id' })
   user: User;
 
-  @ManyToOne(() => Whisper, (whisper) => whisper.rewhispers)
+  @ManyToOne(() => Whisper, (whisper) => whisper.likes)
   @JoinColumn({ name: 'whisper_id' })
   whisper: Whisper;
 }
