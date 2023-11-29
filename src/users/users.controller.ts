994c51a5adcd728b6a5dcda49b20f0dca29569be
hyunjin <alfma123@naver.com>
2023-11-29 16:15:41 +0900

    feat: usersController 에 로그인 controller 추가

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,32 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Request,
+  UseGuards,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from '@src/auth/auth.service';
+import { LocalAuthGuard } from '@src/auth/local-auth.guard';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   async create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
     await this.usersService.signup(createUserDto);
     return 'ok';
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Request() req) {
+    return await this.authService.generateToken(req.user);
   }
 }
