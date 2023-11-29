54d0606530568dfd31997568f2addc4313acd6d7
hyunjin <alfma123@naver.com>
2023-11-29 16:10:29 +0900

    feat: UserModule <> AuthModule간 참조 순환 해소( forwardRef 지정) 및 Jwt모듈, 전략 설정

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '@src/auth/auth.module';
 import { User, UserHashTag } from '@src/models';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserHashTag])],
+  imports: [
+    TypeOrmModule.forFeature([User, UserHashTag]),
+    forwardRef(() => AuthModule),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
