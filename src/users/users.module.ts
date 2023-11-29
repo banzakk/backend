ccd7e8e33bb41df7d03b4be24db6f03b812b36bd
hyunjin <alfma123@naver.com>
2023-11-29 14:29:13 +0900

    feat: UsersService export

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { UsersService } from './users.service';
   imports: [TypeOrmModule.forFeature([User, UserHashTag])],
   controllers: [UsersController],
   providers: [UsersService],
+  exports: [UsersService],
 })
 export class UsersModule {}
