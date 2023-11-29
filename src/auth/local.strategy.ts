ca084fd32ff9f52683baf06c7957d567a79f8e15
hyunjin <alfma123@naver.com>
2023-11-29 14:28:38 +0900

    feat: Local 전략 수립

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,22 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from './auth.service';
+import { AuthInfo } from './types/auth-info';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy) {
+  constructor(private authService: AuthService) {
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
+  }
+  async validate(email: string, password: string): Promise<AuthInfo> {
+    const user = await this.authService.validateUser(email, password);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
