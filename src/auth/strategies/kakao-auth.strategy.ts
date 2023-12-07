9984a7518e13a123ba301f82eb345dca84dff9cd
hyunjin <alfma123@naver.com>
2023-12-08 01:31:47 +0900

    feat: kakao 로그인 전략 추가

---
+++ src/auth/strategies/kakao-auth.strategy.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { UsersService } from '@src/users/users.service';
+import { Strategy } from 'passport-kakao';
+
+@Injectable()
+export class KakaoAuthStrategy extends PassportStrategy(Strategy, 'kakao') {
+  constructor(
+    private configService: ConfigService,
+    private usersService: UsersService,
+  ) {
+    super({
+      clientID: configService.get<string>('KAKAO_CLIENT_ID'),
+      clientSecret: configService.get<string>('KAKAO_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('KAKAO_CALLBACK_URI'),
+      scope: ['account_email', 'profile_nickname'],
+    });
+  }
+
+  async validate(accessToken, refreshToken, profile) {
+    const { _json } = profile;
+    const email = _json.kakao_account.email;
+    const name = _json.kakao_account.profile.nickname;
+
+    let user = await this.usersService.getSocialUserByEmail(email);
+    if (!user) {
+      user = await this.usersService.socialSignUpTransaction(
+        email,
+        name,
+        'kakao',
+      );
+    }
+    const { uid, id } = user;
+
+    return {
+      name,
+      email,
+      uid,
+      id,
+    };
+  }
+}
