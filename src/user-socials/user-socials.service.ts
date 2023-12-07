3b0972f4891455cb17a29bdd4a1aba6b1c0066c5
hyunjin <alfma123@naver.com>
2023-12-07 15:02:43 +0900

    feat: userSocials 모듈, 서비스 추가 및 saveUserSocial 메서드 추가

---
+++ src/user-socials/user-socials.service.ts
@@ -0,0 +1,29 @@
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryRunner, Repository } from 'typeorm';
+import { UserSocial } from './entities/user-social.entity';
+
+@Injectable()
+export class UserSocialsService {
+  constructor(
+    @InjectRepository(UserSocial)
+    private userSocialRepository: Repository<UserSocial>,
+  ) {}
+
+  async saveUserSocial(socialId, userId, queryRunner: QueryRunner) {
+    try {
+      const userSocials = new UserSocial();
+      userSocials.socials = socialId;
+      userSocials.user = userId;
+      if (!queryRunner) {
+        await this.userSocialRepository.save(userSocials);
+        return;
+      }
+      await queryRunner.manager.save(userSocials);
+    } catch (err) {
+      throw new InternalServerErrorException(
+        '소셜 정보 저장 중 오류가 발생했습니다.',
+      );
+    }
+  }
+}
