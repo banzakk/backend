feafc16f166efc99eda0f19b18cae40f38ec49ce
hyunjin <alfma123@naver.com>
2023-12-07 17:14:12 +0900

    refactor: Users 모듈에 UserSocical 엔티티  주입

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@src/auth/auth.module';
 import { UserHashTag } from '@src/models';
 import { SocialsModule } from '@src/socials/socials.module';
+import { UserSocial } from '@src/user-socials/entities/user-social.entity';
 import { UserSocialsModule } from '@src/user-socials/user-socials.module';
 import { User } from '@src/users/entities/user.entity';
 import { UsersController } from './users.controller';
@@ -10,7 +11,7 @@ import { UsersService } from './users.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, UserHashTag]),
+    TypeOrmModule.forFeature([User, UserHashTag, UserSocial]),
     forwardRef(() => AuthModule),
     SocialsModule,
     UserSocialsModule,
