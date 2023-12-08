5048e69e12b96bf5fc97e5b9957c88bc9e5a0a13
hyunjin <alfma123@naver.com>
2023-12-09 03:55:20 +0900

    refactor: https 설정 추가

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('localhost.key'),
+    cert: fs.readFileSync('localhost.crt'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   const configService = app.get(ConfigService);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: 'https://localhost:3000',
     credentials: true,
   });
   app.use(cookieParser());
