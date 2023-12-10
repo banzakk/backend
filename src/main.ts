e135ba3183296297e66a93f8d1fc63ded89b9492
hyunjin <alfma123@naver.com>
2023-12-10 21:46:35 +0900

    refactor: https 설정 일시 삭제

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
-import * as fs from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('localhost.key'),
-    cert: fs.readFileSync('localhost.crt'),
-  };
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   app.enableCors({
