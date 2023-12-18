f74ddf662fda03f1f556fc8ba06e517c47063f9b
hyunjin <alfma123@naver.com>
2023-12-18 17:23:53 +0900

    refactor: follow entity 위치 이동

--- src/follows/entities/follow.entity.ts
+++ src/follows/entities/follow.entity.ts
@@ -1,1 +1,25 @@
-export class Follow {}
+import { User } from '@src/users/entities/user.entity';
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('follows')
+export class Follow {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ManyToOne(() => User, (user) => user.followings)
+  @JoinColumn({ name: 'following_user_id' })
+  follower: User;
+
+  @ManyToOne(() => User, (user) => user.followers)
+  @JoinColumn({ name: 'followed_user_id' })
+  following: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+}
