12cee96ef0d2d927e38b1df904cc8efc0574b5f6
hyunjin <alfma123@naver.com>
2024-01-07 21:12:30 +0900

    fix: 유저 프로필 업데이트시 검증하는 아이디 글자수 수정

--- src/user-profile-images/dto/create-user-profile-image.dto.ts
+++ src/user-profile-images/dto/create-user-profile-image.dto.ts
@@ -9,6 +9,6 @@ export class CreateUserProfileImageDto {
   @IsString(message.typeIs('아이디는', '문자열'))
   @IsNotEmpty(message.notEmpty('아이디는'))
   @MinLength(2, message.minIs('아이디는', 2))
-  @MaxLength(10, message.maxIs('아이디는', 10))
+  @MaxLength(14, message.maxIs('아이디는', 14))
   userCustomId: string;
 }
